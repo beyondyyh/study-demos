5faf8b8601a8297ea9cd8c13035d6ee4e5195a4a
yehong <yehong@staff.weibo.com>
2021-12-15 20:49:56 +0800

    code tidy

--- openresty-demo/lua/wesync/helper/get_config.lua
+++ openresty-demo/lua/wesync/helper/get_config.lua
@@ -5,54 +5,7 @@
 local ngx_say = ngx.say
 local arg = ngx.req.get_uri_args()
 local key = arg.key
-
-local function format_value(val)
-    if type(val) == "string" then
-        return string.format("%q", val)
-    end
-
-    return tostring(val)
-end
-
-local function format_table(t, tabcount)
-    tabcount = tabcount or 0
-    if tabcount > 5 then
-        --防止栈溢出
-        return "<table too deep>" .. tostring(t)
-    end
-    local str = ""
-    if type(t) == "table" then
-        for k, v in pairs(t) do
-            local tab = string.rep("\t", tabcount)
-            if type(v) == "table" then
-                str = str .. tab .. string.format("[%s] = {", format_value(k)) .. '\n'
-                str = str .. format_table(v, tabcount + 1) .. tab .. '}\n'
-            else
-                str = str .. tab .. string.format("[%s] = %s", format_value(k), format_value(v)) .. ',\n'
-            end
-        end
-    else
-        str = str .. tostring(t) .. '\n'
-    end
-
-    return str
-end
-
-local function format(v)
-    if not v then
-        return "nil"
-    end
-
-    if type(v) == 'string' then
-        return format_value(v)
-    end
-
-    if type(v) == "table" then
-        return format_table(v)
-    end
-
-    return v
-end
+local format = require("wesync.tool").format
 
 local wesync_config = require 'wesync.config'
 local dync_config = require 'dync.config'
--- openresty-demo/lua/wesync/tool.lua
+++ openresty-demo/lua/wesync/tool.lua
@@ -32,4 +32,52 @@ function _M.spliturl(path)
     end
 end
 
+local function format_value(val)
+    if type(val) == "string" then
+        return string.format("%q", val)
+    end
+
+    return tostring(val)
+end
+
+local function format_table(t, tabcount)
+    tabcount = tabcount or 0
+    if tabcount > 5 then
+        --防止栈溢出
+        return "<table too deep>" .. tostring(t)
+    end
+    local str = ""
+    if type(t) == "table" then
+        for k, v in pairs(t) do
+            local tab = string.rep("\t", tabcount)
+            if type(v) == "table" then
+                str = str .. tab .. string.format("[%s] = {", format_value(k)) .. '\n'
+                str = str .. format_table(v, tabcount + 1) .. tab .. '}\n'
+            else
+                str = str .. tab .. string.format("[%s] = %s", format_value(k), format_value(v)) .. ',\n'
+            end
+        end
+    else
+        str = str .. tostring(t) .. '\n'
+    end
+
+    return str
+end
+
+function _M.format(v)
+    if not v then
+        return "nil"
+    end
+
+    if type(v) == 'string' then
+        return format_value(v)
+    end
+
+    if type(v) == "table" then
+        return format_table(v, 1)
+    end
+
+    return v
+end
+
 return _M
